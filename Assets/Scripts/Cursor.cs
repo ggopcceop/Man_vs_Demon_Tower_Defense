c100a200270522bc72c2f089523ac7329e50bcc7
Kime <ggopcceop@gmail.com>
2013-10-23 02:08:00 -0300

    Basic cursor hiding and camera controler

---
+++ Assets/Scripts/Cursor.cs
@@ -0,0 +1,50 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class Cursor : MonoBehaviour {
+	
+	public Vector2 mousePosition;
+	public float mouseSensitivity = 15.0f;
+	public Texture cursor;
+	
+	// Use this for initialization
+	void Start () {
+		mousePosition.x = Screen.width / 2;
+		mousePosition.y = Screen.height / 2;
+		
+		Screen.lockCursor = true;
+		Screen.showCursor = false;
+	}
+	
+	// Update is called once per frame
+	void FixedUpdate () {
+		Screen.lockCursor = true;
+		
+		float mDeltaX = Input.GetAxis ("Mouse X");
+		float mDeltaY = Input.GetAxis ("Mouse Y");
+		
+		mousePosition += new Vector2(mDeltaX, -mDeltaY) * mouseSensitivity;
+		
+		if(mousePosition.x < 0){
+			mousePosition.x = 0;
+		}else if(mousePosition.x > Screen.width){
+			mousePosition.x = Screen.width;
+		}
+		
+		if(mousePosition.y < 0){
+			mousePosition.y = 0;
+		}else if(mousePosition.y > Screen.height){
+			mousePosition.y = Screen.height;
+		}
+		
+	}
+	
+	void OnGUI(){
+		GUI.Label(new Rect(mousePosition.x, mousePosition.y, 35, 35), cursor);
+	}
+	
+	void OnMouseDown() {
+        Screen.lockCursor = true;
+		Screen.showCursor = false;
+    }
+}
--- Assets/Scripts/InputControler.cs
+++ Assets/Scripts/InputControler.cs
@@ -2,14 +2,34 @@
 using System.Collections;
 
 public class InputControler : MonoBehaviour {
-
+	
+	public float cameraTiggerWidth = 10.0f;
+	public float cameraSpeed = 12.0f;
+	
+	private GameObject mainCamera;
+	private Cursor cursor;
+	
 	// Use this for initialization
 	void Start () {
-	
+		mainCamera = GameObject.Find("Camera");	
+		cursor = GameObject.Find("Cursor").GetComponent<Cursor>();
+		
 	}
 	
 	// Update is called once per frame
 	void Update () {
-	
+		
+		float mPosX = cursor.mousePosition.x;
+		float mPosY = cursor.mousePosition.y;
+		
+		Vector3 cameraMoveDirection = Vector3.zero;
+		if((mPosX < cameraTiggerWidth) || (mPosX > Screen.width - cameraTiggerWidth)
+			||(mPosY < cameraTiggerWidth) ||(mPosY > Screen.height - cameraTiggerWidth)){
+			cameraMoveDirection = new Vector3(mPosX / Screen.width - 0.5f, 0, -(mPosY / Screen.height - 0.5f));
+			
+		}
+		
+		mainCamera.transform.Translate(cameraMoveDirection * Time.deltaTime * cameraSpeed,Space.World);
+		
 	}
 }
