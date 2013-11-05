cd10d9bc602dad61a503968d440352a44be19b12
Scott <462113164@qq.com>
2013-11-05 14:42:56 -0400

    Add NGUI asset

---
+++ Assets/Scripts/GUI/Driver.cs
@@ -0,0 +1,18 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class Driver : MonoBehaviour {
+	public bar bar;
+	public string str;
+	public int curValue;
+	public int maxValue;
+	
+	public bool displayText = false;
+	
+	void OnClick(){
+		if (!displayText)
+			bar.UpdateDisplay((float)curValue/maxValue);
+		else
+			bar.UpdateDisplay((float)curValue/maxValue,curValue+"/"+maxValue);
+	} 
+}
---
+++ Assets/Scripts/GUI/bar.cs
@@ -0,0 +1,48 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class bar : MonoBehaviour {
+	private UISlider slider;
+	private float maxWidth;
+	public UILabel label;
+	private bool displayText = false;
+	void Awake(){
+		slider = GetComponent<UISlider>();
+		if (slider == null){
+			Debug.LogError("UISlider cannot found");
+			return;
+		}
+		maxWidth = slider.foreground.localScale.x;
+		
+		DisplayText = displayText;
+	}
+	
+	void Start(){
+		//UpdateDisplay(.5f);
+	}
+	
+	public void UpdateDisplay(float x){
+		if (x<0){
+			x=0;
+		}
+		else if (x>1){
+			x=1;
+		}
+		slider.foreground.localScale = new Vector3(maxWidth*x,slider.foreground.localScale.y,slider.foreground.localScale.z);
+		DisplayText = false;
+	}
+	public void UpdateDisplay(float x, string str){
+		UpdateDisplay(x);
+		if (str!="")
+			label.text = str;
+	}
+	public bool DisplayText{
+		get{return displayText;}
+		set{
+			displayText = value;
+			if(!displayText){
+				label.text = "";
+			}
+		}
+	}
+}
